1efd35d5ad192df12857d3a91af7a24433ebfe61
simon <dlgmlals3@gmail.com>
2023-04-17 22:45:36 +0900

    Vector magnitude normalization

--- Geometry/Vector.cpp
+++ Geometry/Vector.cpp
@@ -1,1 +1,17 @@
 #include "Vector.h"
+using namespace jmk;
+
+float corssProduct2d(Vector2f v1, Vector2f v2)
+{
+	return v1[X] * v2[Y] - v1[Y] * v2[X];
+}
+
+Vector3f crossProduct3d(Vector3f v1, Vector3f v2) 
+{
+	Vector3f a = { (v1[Y] * v2[Z] - v1[Z] * v2[Y]) , 
+		(v1[Z] * v2[X] - v1[X] * v2[Z]) , 
+		(v1[X] * v2[Y] - v1[Y] * v2[X]) };
+	return a;
+}
+
+
